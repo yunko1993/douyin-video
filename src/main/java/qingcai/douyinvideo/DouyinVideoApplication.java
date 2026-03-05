0053b0d28d3d48859ddc4acaaa0da282ba98a4cf
qingcai <1023902556@qq.com>
2026-03-05 11:28:36 +0800

    feat: 覆盖远程，以本地代码为准

---
+++ src/main/java/qingcai/douyinvideo/DouyinVideoApplication.java
@@ -0,0 +1,13 @@
+package qingcai.douyinvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouyinVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DouyinVideoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qingcai/douyinvideo/DouyinVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package qingcai.douyinvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DouyinVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
